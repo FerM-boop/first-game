a7a7d37359982ff7b44575fe5caf7cf0526e8f91
Fer-Macbook <fermejorado1@gmail.com>
2023-02-19 14:43:25 -0600

    Added bombs, they instakill Hungry Joe. Bombs have sound. Game Over alert reloads game on click.

--- game.js
+++ game.js
@@ -1,9 +1,26 @@
 class mainScene {
   preload() {
     this.load.image('player', 'assets/player.png');
+
     this.load.image('croissant', 'assets/croissant.png');
+
+    this.load.image('bomb1', 'assets/bomb.png');
+    this.load.image('bomb2', 'assets/bomb.png');
+    this.load.image('bomb3', 'assets/bomb.png');
+    this.load.image('bomb4', 'assets/bomb.png');
+    this.load.image('bomb5', 'assets/bomb.png');
+
   }
   
+  bomb(bombnum) {
+    if (this.started) {
+      var audio = new Audio("./assets/soundfx/explosion.mp3");
+      audio.play();
+      bombnum.destroy(true);
+      setTimeout(function(){alert("Game Over!"); location.reload()},50)
+    }
+  }
+
   hit() {
     var audio = new Audio("./assets/soundfx/croissant_sound.mp3");
     audio.play();
@@ -23,6 +40,17 @@ class mainScene {
     
     this.croissant = this.physics.add.sprite(300, 300, 'croissant');
     this.croissant.setDisplaySize(35,35)
+
+    this.bomb1 = this.physics.add.sprite(Phaser.Math.Between(100, 600), Phaser.Math.Between(100, 300), 'bomb1');
+    this.bomb1.setDisplaySize(35,35)
+    this.bomb2 = this.physics.add.sprite(Phaser.Math.Between(100, 600), Phaser.Math.Between(100, 300), 'bomb1');
+    this.bomb2.setDisplaySize(35,35)
+    this.bomb3 = this.physics.add.sprite(Phaser.Math.Between(100, 600), Phaser.Math.Between(100, 300), 'bomb1');
+    this.bomb3.setDisplaySize(35,35)
+    this.bomb4 = this.physics.add.sprite(Phaser.Math.Between(100, 600), Phaser.Math.Between(100, 300), 'bomb1');
+    this.bomb4.setDisplaySize(35,35)
+    this.bomb5 = this.physics.add.sprite(Phaser.Math.Between(100, 600), Phaser.Math.Between(100, 300), 'bomb1');
+    this.bomb5.setDisplaySize(35,35)
     
     this.score = 0;
     this.started = false;
@@ -49,7 +77,18 @@ class mainScene {
       this.started = true;
     } else if (this.physics.overlap(this.player, this.croissant)) {
       this.hit();
+    } else if (this.physics.overlap(this.player, this.bomb1)) {
+      this.bomb(this.bomb1);
+    } else if (this.physics.overlap(this.player, this.bomb2)) {
+      this.bomb(this.bomb2);
+    } else if (this.physics.overlap(this.player, this.bomb3)) {
+      this.bomb(this.bomb3);
+    } else if (this.physics.overlap(this.player, this.bomb4)) {
+      this.bomb(this.bomb4);
+    } else if (this.physics.overlap(this.player, this.bomb5)) {
+      this.bomb(this.bomb5);
     }
+
   }
 }
 
